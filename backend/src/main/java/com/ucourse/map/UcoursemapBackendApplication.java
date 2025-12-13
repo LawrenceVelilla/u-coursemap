891d4176901cff18d2a6e0c81d61cf8e2af5bf5b
LawrenceVelilla <velilla.lawrencePHC@gmail.com>
2025-12-13 00:32:40 -0700

    java deps

---
+++ backend/src/main/java/com/ucourse/map/UcoursemapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ucourse.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcoursemapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcoursemapBackendApplication.class, args);
+	}
+
+}
